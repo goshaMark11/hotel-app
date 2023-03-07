22602b289ae76390c21db5eabe2f35302b3735a9
Gregory Marchenkov <wowg195@gmail.com>
2023-03-07 15:51:22 +0300

    test3

--- src/store/sagaActions/hotel-actions.ts
+++ src/store/sagaActions/hotel-actions.ts
@@ -2,4 +2,4 @@ import { FetchHotelsData } from '../../services/hotel/types/fetch-hotels-data'
 
 export const FETCH_HOTELS_SAGA = 'FETCH_HOTELS_SAGA'
 
-export const fetchHotels = (fetchHotelsData: FetchHotelsData) => ({ type: FETCH_HOTELS_SAGA, payload: { ...fetchHotelsData } } )
+export const fetchHotels = (fetchHotelsData: FetchHotelsData) => ({ type: FETCH_HOTELS_SAGA, payload: { ...fetchHotelsData } }  )
